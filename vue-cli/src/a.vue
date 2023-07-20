09551dc2dea3ee92e9d98e1c5751f4be5a3244f3
minyugyeong <alsdbrud123@naver.com>
2023-07-20 18:09:48 +0900

    vue-cli 생성

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,43 @@
+var appHeader = {
+  template: '<div>header</div>',
+  methods: {
+    addNum: function(){
+      
+    }
+  }
+}
+
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  // Javascript - 인스턴스 옵션
+  methods: {
+    addNum: function(){
+
+    }
+  },
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
