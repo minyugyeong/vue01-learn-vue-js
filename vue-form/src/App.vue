1ad9fa40cebb3bce1adec8f63f5daed6b60ff7a7
minyugyeong <alsdbrud123@naver.com>
2023-07-21 10:46:03 +0900

    vue-form

---
+++ vue-form/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input type="text" id="username" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input type="password" id="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  
+
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    submitForm: function(){
+      console.log(this.username, this.password);
+      var url = "https://jsonplaceholder.typicode.com/users";
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+      .then(function(response){
+        console.log(response.data);
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+    }
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
