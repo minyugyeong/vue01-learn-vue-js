1ac6552db75bddf99f46ffb0f0e034333c4768ef
minyugyeong <alsdbrud123@naver.com>
2023-07-21 09:40:39 +0900

    vue-cli

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,34 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <!-- 컴포넌트 명명법 종류 -->
-    <!-- <hello-word></hello-word>
-    <HelloWorld></HelloWorld>
-    <HelloWorld/> -->
+  <div>
+    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
 
 export default {
-  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: 'App',
+
+  data() {
+    return {
+      str: 'Header'
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    renewStr: function(){
+      this.str = 'hi';
+    }
+  },
+
   components: {
-    HelloWorld,
-    'hello-world': HelloWorld,
+    'app-header': AppHeader
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{ propsdata }}</h1>
+    <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ['propsdata'],
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    sendEvent: function(){
+      this.$emit('renew')
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
